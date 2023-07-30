af6559eb87e49ffe10dcbba95ee9d22eff782fe9
ADMIN <ADMIN@DESKTOP-QPJF32R>
2023-07-30 23:35:19 +0530

    first code

---
+++ src/Javapracticee/test.java
@@ -0,0 +1,11 @@
+package Javapracticee;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello JAVA It's my first code");
+	}
+
+}
