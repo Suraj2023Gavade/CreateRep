9f1e53dd20f304311711fb2d55a3f7b1154cfc72
ADMIN <ADMIN@DESKTOP-QPJF32R>
2023-08-06 09:46:01 +0530

    commit swap code

---
+++ src/Javapracticee/Swap.java
@@ -0,0 +1,26 @@
+package Javapracticee;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 10;
+
+	    int j = 7;
+	    
+	    int k;
+	    k=i;
+	    i=j;
+	    j=k;
+	    
+	    // Write your code
+
+	    System.out.println("Value of i ="+i);
+
+	    System.out.println("Value of j ="+j);
+
+	    // Expected Result , i = 7  and j = 10
+	}
+
+}
---
+++ src/Javapracticee/check.java
@@ -0,0 +1,13 @@
+package Javapracticee;
+
+class check
+{
+
+	public static void main(String[] args) 
+       {
+		
+
+		System.out.println("Hello Java it's check code"); 
+       }
+	}
+
