aeb6776c7ff2691f3312bf507c1e9171ccda10a7
ADMIN <ADMIN@DESKTOP-QPJF32R>
2023-08-06 10:23:26 +0530

    Sawp without temp value

---
+++ src/Javapracticee/Swapwithouttemp.java
@@ -0,0 +1,23 @@
+package Javapracticee;
+
+public class Swapwithouttemp {
+
+	public static void main(String[] args) 
+	{
+		
+		int i = 10;
+
+	    int j = 7;
+
+	    i = i+j;
+	    j = i-j;
+	    i = i-j;
+
+	    System.out.println("Value of i = " +i);
+
+	    System.out.println("Value of i = "+j);
+	    
+		
+	}
+
+}
